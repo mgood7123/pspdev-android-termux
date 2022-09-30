f9bf86ecd43b2368c4d71c9217ccb257a7ff59ba
mgood7123 <smallville7123@gmail.com>
2022-09-30 11:36:38 +1000

    Enable psp debugging \o/

--- install-pspdev.sh
+++ install-pspdev.sh
@@ -1,58 +1,53 @@
 #!/data/data/com.termux/files/usr/bin/bash
-folder=ubuntu-pspdev-fs
+case `dpkg --print-architecture` in
+  aarch64)
+    psparch="aarch64"
+    linarch="arm64"
+    ;;
+#  arm)
+#		 psparch="armv7"
+#    linarch="armhf"
+#    ;;
+	*)
+			echo "unsupported architecture, we only support aarch64 for now"
+    exit 1
+    ;;
+esac
+
+folder="ubuntu-$linarch--pspdev-$psparch--rootfs"
 uv=22.04
-tarball="ubuntu${uv}.tar.gz"
-if [ "$first" != 1 ];then
-	if [ ! -f $tarball ]; then
-		echo "downloading ubuntu-image"
-		case `dpkg --print-architecture` in
-		aarch64)
-			archurl="arm64" ;;
-		arm)
-			archurl="armhf" ;;
-		amd64)
-			archurl="amd64" ;;
-		i*86)
-			archurl="i386" ;;
-		x86_64)
-			archurl="amd64" ;;
-		*)
-			echo "unknown architecture"; exit 1 ;;
-		esac
-		wget "https://cdimage.ubuntu.com/ubuntu-base/releases/${uv}/release/ubuntu-base-${uv}-base-${archurl}.tar.gz" -O "$tarball"
-	fi
-	cur=`pwd`
+tarball="ubuntu.tar.gz"
+
+echo "downloading ubuntu-image"
+wget "https://cdimage.ubuntu.com/ubuntu-base/releases/${uv}/release/ubuntu-base-${uv}-base-${linarch}.tar.gz" -O "$tarball"
+cur=`pwd`
 echo removing any existing "$folder"
-  rm -rf "$folder"
-	mkdir -p "$folder"
-	cd "$folder"
-	echo "decompressing ubuntu image"
-	proot --link2symlink tar -xf "${cur}/${tarball}" --exclude='dev'||:
-echo removing ubunto image tarball
+rm -rf "$folder"
+mkdir -p "$folder"
+cd "$folder"
+echo "decompressing ubuntu image"
+proot --link2symlink tar -xf "${cur}/${tarball}" --exclude='dev'||:
+echo "removing ubuntu image tarball"
 rm "${cur}/${tarball}"
-	echo "fixing nameserver, otherwise it can't connect to the internet"
-	echo "nameserver 1.1.1.1" > etc/resolv.conf
-	cd "$cur"
-fi
-mkdir -p binds
-bin=start-ubuntu-pspdev.sh
+echo "fixing nameserver, otherwise it can't connect to the internet"
+echo "nameserver 1.1.1.1" > etc/resolv.conf
+echo "127.0.0.1 localhost" > etc/hosts
+cd "$cur"
+bin=enter_ubuntu.sh
 echo "writing launch script"
 cat > $bin <<- EOM
 #!/bin/bash
-cd \$(dirname \$0)
+cd "$cur"
 ## unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
-command="proot"
+echo "root is required to enable psp debugging"
+command="su -c /data/data/com.termux/files/usr/bin/proot"
 command+=" --link2symlink"
 command+=" -0"
 command+=" -r $folder"
-if [ -n "\$(ls -A binds)" ]; then
-    for f in binds/* ;do
-      . \$f
-    done
-fi
 command+=" -b /dev"
 command+=" -b /proc"
+command+=" -b /sys"
 ## uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## uncomment the following line to mount /sdcard directly to / 
@@ -77,17 +72,25 @@ termux-fix-shebang $bin &&
 echo "making $bin executable" &&
 chmod +x $bin &&
 echo symlinking sh to bash &&
-./$bin "rm /bin/sh; ln -s bash /bin/sh" &&
+./$bin "rm /bin/sh; ln -s /bin/bash /bin/sh" &&
+echo "updating package list" &&
+./$bin "apt update" &&
+echo "installing core packages for apt" &&
+./$bin "apt install -y apt-utils" &&
+./$bin "apt install -y dialog" &&
+./$bin "apt install -y sudo" &&
+./$bin "apt install -y wget curl tzdata" &&
+echo "installing dependancies" &&
+./$bin "curl -L https://raw.githubusercontent.com/pspdev/pspdev/master/prepare-debian-ubuntu.sh > i.sh && chmod +x ./i.sh && ./i.sh -y && rm i.sh" &&
+echo "downloading pspdev" &&
+wget https://github.com/pspdev/pspdev/releases/download/v20220202/pspdev-ubuntu-${uv}-${psparch}.tar.gz -O "$cur/pspdev.tar.gz" &&
+echo "extracting pspdev..." &&
+cd "$folder/usr/local/" &&
+tar -xzf "$cur/pspdev.tar.gz" &&
+rm "$cur/pspdev.tar.gz" &&
+cd "$cur" &&
+./$bin "unminimize"
 echo "exporting needed environmental variables for pspdev" &&
-echo "export PSPDEV=/PSPDEV-ANDROID" >> "$folder/root/.bashrc" &&
-echo "export PATH=/PSPDEV-ANDROID/bin:\$PATH" >> "$folder/root/.bashrc" &&
-echo "updating apt for pspdev" &&
-./$bin "apt update && apt install -y dialog && apt upgrade -y" &&
-echo "installing packages for pspdev" &&
-./$bin "apt install -y sudo git libipt* python2 libdebuginfo*" &&
-echo cloning pspdev &&
-./$bin git clone https://www.github.com/pspdev/pspdev &&
-echo preparing pspdev &&
-./$bin "cd pspdev ; ./prepare-debian-ubuntu.sh" &&
+echo "export PSPDEV=/usr/local/pspdev" >> "$folder/root/.bashrc" &&
+echo "export PATH=/usr/local/pspdev/bin:\$PATH" >> "$folder/root/.bashrc" &&
 echo "You can now launch Ubuntu pspdev with the ./${bin} script"
-echo "after launching, please cd pspdev; ./build-all.sh"
